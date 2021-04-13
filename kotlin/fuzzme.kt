ffaabd1cb85d946b643f0234c5a153826d6b8afb
Andrew Yang <andrew@Andrews-MacBook-Pro.local>
2021-04-13 15:38:44 -0400

    adding updates

--- kotlin/fuzzme.kt
+++ kotlin/fuzzme.kt
@@ -1,3 +1,18 @@
-fun main() {
-    println("Hello, World!")
+package fuzzme;
+
+public class FuzzMe {
+    fun fuzzerInitialize(){
+        // any state initialization goes here!
+    }
+
+    fun fuzzerTestOneInput(data: ByteArray) {
+        val input = String(data)
+        if (input.startsWith("b", 0)) {
+            if (input.startsWith("u", 1)) {
+                if (input.startsWith("g", 2)) {
+                    val i: Int = 1 / 0;
+                }
+            }
+        }
+    }
 }
