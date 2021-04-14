d7bc303f99c83457db6b03188f41911d160a13d0
Andrew Yang <andrew@Andrews-MacBook-Pro.local>
2021-04-14 12:59:16 -0400

    adjusting fuzzme.rs

--- rust/rust-cargo-fuzz/fuzzme.rs
+++ rust/rust-cargo-fuzz/fuzzme.rs
@@ -1,32 +1,15 @@
-use std::fs::File;
-use std::io;
-use std::io::prelude::*;
-use std::env;
+#![no_main]
+#[macro_use] extern crate libfuzzer_sys;
 
-fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
-
-    let mut f = File::open(filename)?;
-    let mut buffer = [0; 10];
-
-    // read up to 10 bytes
-    f.read(&mut buffer)?;
-    bad(&buffer);
-    Ok(())
-}
-
-fn bad(data: &[u8]) {
+fuzz_target!(|data: &[u8]| {
     if let Ok(contents) = std::str::from_utf8(data) {
-        if contents.starts_with("b") {
-            if contents[1..].starts_with("u") {
-                if contents[2..].starts_with("g") {
-                    div_by_val(0);
-                }
-            }
-        }
+       if contents.contains("bad") {
+           div_by_val(0);
+       } else {
+           div_by_val(1);
+       }
     }
-}
+});
 
 fn div_by_val(num: i32) -> i32 {
     return 1/num;
