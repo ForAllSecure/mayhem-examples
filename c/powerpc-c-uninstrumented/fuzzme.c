5228db024c21cb3fc16c8dc70f4f6283552c5be7
Eddy Mulyono <eddymul@forallsecure.com>
2021-04-14 14:43:52 -0700

    c/powerpc-c-uninstrumented/: Add

---
+++ c/powerpc-c-uninstrumented/fuzzme.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+int fuzzme(char *buf, unsigned len);
+
+int main(int argc, char *argv[])
+{
+  FILE *f;
+  char buf[12];
+
+  if(argc != 2){
+    fprintf(stderr, "Must supply a text file\n");
+    return -1;
+  }
+  f = fopen(argv[1], "r");
+  if(f == NULL){
+    fprintf(stderr, "Could not open %s\n", argv[1]);
+    return -1;
+  }
+  if(fgets(buf, sizeof(buf), f) == NULL){
+    fprintf(stderr, "Could not read from %s\n", argv[1]);
+    return -1;
+  }
+  fuzzme(buf, strlen(buf));
+  return 0;
+}
+
+int fuzzme(char *buf, unsigned len)
+{
+  unsigned ok;
+
+  if(!ok) // Defect: uninitialized use of ok.
+    ok = len;
+
+  if(buf[0] == 'b')
+    if(buf[1] == 'u')
+      if(buf[2] == 'g') {
+        return 1/0;      // Defect: divide-by-zero.
+      }
+  return 0;
+}
