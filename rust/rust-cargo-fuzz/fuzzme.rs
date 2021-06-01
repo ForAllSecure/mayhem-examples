a02ef58a02dff59a089162d7dfabba1e5592e44a
Andrew Yang <andrew@Andrews-MacBook-Pro.local>
2021-06-01 17:08:35 -0400

    updating line

--- rust/rust-cargo-fuzz/fuzzme.rs
+++ rust/rust-cargo-fuzz/fuzzme.rs
@@ -2,7 +2,7 @@
 #[macro_use] extern crate libfuzzer_sys;
 
 fuzz_target!(|data: &[u8]| {
-    if data.len() >= 3 && data.len() < 5 {
+    if data.len() >= 3 {
         if data[0] == 'b' as u8 {
             if data[1] == 'u' as u8 {
                 if data[2] == 'g' as u8 {
