2ae40e450890a40041c2e5b30c7c45f3fce50022
Andrew Yang <andrewyang@forallsecure.com>
2021-06-11 12:08:54 -0400

    adding edits

--- c/powerpc64le-c-uninstrumented/fuzzme.c
+++ c/powerpc64le-c-uninstrumented/fuzzme.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
-int fuzzme(char *buf, unsigned len);
+int fuzzme(char *buf)
+{
+  if(buf[0] == 'b')
+    if(buf[1] == 'u')
+      if(buf[2] == 'g') {
+	      *(int *)0x0 = 1; // Defect: null pointer dereference
+      }
+  return 0;
+}
 
 int main(int argc, char *argv[])
 {
@@ -21,21 +29,6 @@ int main(int argc, char *argv[])
     fprintf(stderr, "Could not read from %s\n", argv[1]);
     return -1;
   }
-  fuzzme(buf, strlen(buf));
-  return 0;
-}
-
-int fuzzme(char *buf, unsigned len)
-{
-  unsigned ok;
-
-  if(!ok) // Defect: uninitialized use of ok.
-    ok = len;
-
-  if(buf[0] == 'b')
-    if(buf[1] == 'u')
-      if(buf[2] == 'g') {
-	*(int *)0x0 = 1; // Defect: null pointer dereference
-      }
+  fuzzme(buf);
   return 0;
 }
