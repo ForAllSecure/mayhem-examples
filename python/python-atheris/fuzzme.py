d4683e886d925545a5aa9407cea9f77682830079
Andrew Yang <andrewyang@forallsecure.com>
2021-06-14 16:59:47 -0400

    adding changes to python fuzzme example

--- python/python-atheris/fuzzme.py
+++ python/python-atheris/fuzzme.py
@@ -3,13 +3,13 @@
 import atheris
 import sys
 
-
 def TestOneInput(data):
-    if data.startswith(b"b"):
-        if data[1:].startswith(b"u"):
-            if data[2:].startswith(b"g"):
-                return 1 / 0
-
+    if len(data) >= 3 :    
+        if data[0] == ord('b'):
+            if data[1] == ord('u'):
+                if data[2] == ord('g'):
+                    print("Made it to the bug!")
+                    return 1 / 0
 
 atheris.Setup(sys.argv, TestOneInput)
 atheris.Fuzz()
