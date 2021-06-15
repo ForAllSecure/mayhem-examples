592e58c8399807560f5afa3da5f42e48866926a9
Andrew Yang <andrewyang@forallsecure.com>
2021-06-15 13:35:04 -0400

    fixing whitespace

--- go/go-base-executable/fuzzme.go
+++ go/go-base-executable/fuzzme.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "os"
 )
 
 func fuzzme(bytes []byte) int {
-	content := string(bytes)
-	if len(content) >= 3 {
-		if string(content[0]) == "b" {
-			if string(content[1]) == "u" {
-				if string(content[2]) == "g" {
-					_ = divBy(0)
-				}
-			}
-		}
-	}
-	return 0
+    content := string(bytes)
+    if len(content) >= 3 {
+        if string(content[0]) == "b" {
+            if string(content[1]) == "u" {
+                if string(content[2]) == "g" {
+                    _ = divBy(0)
+                }
+            }
+        }
+    }
+    return 0
 }
 
 func divBy(val int) int {
-	y := 1 / val
-	return y
+    y := 1 / val
+    return y
 }
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Printf("Usage: ./fuzzme <file>\n")
-		os.Exit(1)
-	}
-	bytes, err := ioutil.ReadFile(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-	fuzzme(bytes)
+    if len(os.Args) != 2 {
+        fmt.Printf("Usage: ./fuzzme <file>\n")
+        os.Exit(1)
+    }
+    bytes, err := ioutil.ReadFile(os.Args[1])
+    if err != nil {
+        log.Fatal(err)
+    }
+    fuzzme(bytes)
 }
