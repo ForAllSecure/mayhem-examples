60ca81674acdb1fd1c3abd3096e710978b784e73
Andrew Yang <andrewyang@forallsecure.com>
2021-06-19 21:04:23 -0400

    edit

--- cpp/cpp-libfuzzer/fuzzme.cpp
+++ cpp/cpp-libfuzzer/fuzzme.cpp
@@ -9,7 +9,7 @@ int fuzzme(char *buf, unsigned len)
       if(buf[1] == 'u')
         if(buf[2] == 'g') {
           printf("You've got it!");
-          return 1/0;      // Defect: divide-by-zero.
+          return buf[0]/(buf[0] - 'b');      // Defect: divide-by-zero.
         }
   return 0;
 }
