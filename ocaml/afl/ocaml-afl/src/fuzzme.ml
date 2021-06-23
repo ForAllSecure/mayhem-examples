b6a03570fbf49e7032e571331321c1b9c04da9ce
Andrew Yang <andrewyang@forallsecure.com>
2021-06-23 17:48:17 -0400

    adding min length checker

--- ocaml/afl/ocaml-afl/src/fuzzme.ml
+++ ocaml/afl/ocaml-afl/src/fuzzme.ml
@@ -12,9 +12,10 @@ let notify_mayhem () = Unix.kill (Unix.getpid ()) Sys.sigabrt
 let main () =
   let ic = open_in Sys.argv.(1) in
   let s = input_line ic in
-  match Array.to_list (Array.init (String.length s) (String.get s)) with
-    ['b'; 'u'; 'g'] -> notify_mayhem ()
-  | _ -> assert(String.length s >= 3)
+  if String.length(s) >= 3 then
+    match Array.to_list (Array.init (String.length s) (String.get s)) with
+      ['b'; 'u'; 'g'] -> notify_mayhem ()
+  | _ -> ()
 
 
 let _ =
