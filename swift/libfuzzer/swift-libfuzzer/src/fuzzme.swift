baaa1dd4c886df0e2c7887d3f1c42b12722d282c
Andrew Yang <andrewyang@forallsecure.com>
2021-06-29 16:43:39 -0400

    adding files

---
+++ swift/libfuzzer/swift-libfuzzer/src/fuzzme.swift
@@ -0,0 +1,14 @@
+@_cdecl("LLVMFuzzerTestOneInput") public func fuzzMe(data: UnsafePointer<CChar>, size: CInt) -> CInt {
+    let buffer = UnsafeBufferPointer(start: data, count: Int(size))
+    guard buffer.count >= 3 else {
+        return 1
+    }
+    if
+        buffer[0] == 0x62, // b
+        buffer[1] == 0x75, // u
+        buffer[2] == 0x67  // g
+    {
+        fatalError()
+    }
+    return 0
+}
