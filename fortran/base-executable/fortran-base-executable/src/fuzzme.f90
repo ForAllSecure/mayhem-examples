7b1896c54fc799cd9148941f06a0fe86133ece01
Andrew Yang <andrewyang@forallsecure.com>
2021-06-30 14:38:25 -0400

    adding max's edits

--- fortran/base-executable/fortran-base-executable/src/fuzzme.f90
+++ fortran/base-executable/fortran-base-executable/src/fuzzme.f90
@@ -1,14 +1,15 @@
 CHARACTER(len=128) :: filename
 CHARACTER(len=10) :: input
-INTEGER(4) I
+INTEGER(4) :: stat
+
 IF(COMMAND_ARGUMENT_COUNT().NE.1)THEN
      WRITE(*,*)'ERROR, NEED INPUT AS ARGUMENT'
      STOP
 ENDIF
- 
+
 CALL GET_COMMAND_ARGUMENT(1, filename) ! read in input
-Open(1, file=filename)
-Read(1,*) input
+Open(1, FILE=filename, ACCESS='STREAM', FORM='UNFORMATTED')
+Read(1, IOSTAT=stat) input
 IF (LEN(input) >= 3) THEN
      IF (input(1:1) == 'b') THEN
           if(input(2:2) == 'u') THEN
@@ -18,6 +19,5 @@ IF (LEN(input) >= 3) THEN
           ENDIF
      ENDIF
 ENDIF
-WRITE(*,*)input ! this is ok.
- 
+WRITE(*,*) input ! this is ok.
 END PROGRAM
