3519b5db0844d643c443610f1a49d49b49ef378c
Andrew Yang <andrewyang@forallsecure.com>
2022-01-28 12:08:43 -0500

    updating defects to uncaught exception

--- java/libfuzzer/java-jazzer/src/FuzzMe.java
+++ java/libfuzzer/java-jazzer/src/FuzzMe.java
@@ -7,7 +7,7 @@ public class FuzzMe {
             if (input.charAt(0) == 'b') {
                 if (input.charAt(1) == 'u') {
                     if (input.charAt(2) == 'g') {
-                        System.exit(-1);
+                        throw new RuntimeException("Made it to the bug!");
                     }
                 }
             }
