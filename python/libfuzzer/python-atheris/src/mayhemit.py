939d5de57f49e2877c3d7448aa1c4ea470c7a664
Andrew Yang <andrewyang@forallsecure.com>
2022-10-13 16:31:20 -0400

    fixing shebang

--- python/libfuzzer/python-atheris/src/mayhemit.py
+++ python/libfuzzer/python-atheris/src/mayhemit.py
@@ -1,11 +1,11 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 
 import atheris
 import sys
 import os
 
 def TestOneInput(data):
-    if len(data) >= 3 :    
+    if len(data) >= 3 :
         if data[0] == ord('b'):
             if data[1] == ord('u'):
                 if data[2] == ord('g'):
